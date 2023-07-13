88fd63547148ec168dbe6ff450394d6adf45f8f9
Щукин Павел Владимирович <p_shchukin@RTKIT.local>
2023-07-13 21:28:14 +0300

    Made all tasks

---
+++ task02/Program.cs
@@ -0,0 +1,27 @@
+﻿// Задача 21: Напишите программу, которая принимает на вход координаты двух точек и находит расстояние между ними в 2D
+// пространстве.
+// A (3,6); B (2,1) -> 5,09
+// A (7,-5); B (1,-1) -> 7,21
+
+int Coordinata(string message)// Этот медод для того, чтобы не писать Console.ReadLine несколько раз
+{
+    Console.WriteLine(message);
+    int num = int.Parse(Console.ReadLine()!);// Запрашивает у пользователя данные
+    return num;// возвращает их обратно нам в работу
+}
+
+double Distance(int a, int b, int c, int d)
+{
+    double distance = Math.Sqrt((a - c) * (a - c) + (b - d) * (b - d));
+    return distance;
+}
+
+int x1 = Coordinata("Введите первую координату точки 1: ");
+int y1 = Coordinata("Введите первую координату точки 1: ");
+int x2 = Coordinata("Введите первую координату точки 2: ");
+int y2 = Coordinata("Введите первую координату точки 2: ");
+
+//double d = Math.Sqrt((x1 - x2) * (x1 - x2) + (y1 - y2) * (y1 - y2));// Решение без второго метода 
+Console.WriteLine("Расстоение между точками: " + Math.Round(Distance(x1, y1, x2, y2), 3));// вывод сразу подставляет в метод данные и выводим в консоле
+//и через Math.Round(число, 3) округляем до 3-х знаков после запятой.
+
---
+++ task04/Program.cs
@@ -0,0 +1,28 @@
+﻿// Задача 22: Напишите программу, которая принимает на вход число (N) и выдаёт таблицу квадратов чисел от 1 до N.
+// 5 -> 1, 4, 9, 16, 25.
+// 2 -> 1,4
+
+int GetNumber(string message)// Этот медод для того, чтобы не писать Console.ReadLine несколько раз
+{
+    Console.WriteLine(message);
+    int num = int.Parse(Console.ReadLine()!);// Запрашивает у пользователя данные
+    return num;// возвращает их обратно нам в работу
+}
+
+void Square(int num)//Печатаем квадраты числа от 1 до N
+{
+int count = 1;
+while(count <= num)
+{
+    Console.Write(count * count + " ");
+    count++;
+    
+};
+}
+int N = GetNumber("Введите число: ");
+Square(N);
+
+
+
+    
+
